dea4fd78085b0e7f34b384f37be51fdc059ad998
Parag <psychoprg357@gmail.com>
2020-05-03 16:56:44 +0530

    minor changes

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".single-image").mouseover(function(){
+    $(".single-image").click(function(){
       var t = $(this).attr("src");
       $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
       $("#myModal").modal();
